acbc63805ed47d73e6f8eebe90240dfb4a3395e8
Pasi Hanhijärvi <pasi.hanhijarvi@gmail.com>
2017-08-28 01:00:31 +0300

    finished ex13

--- ex13/Exercise13.java
+++ ex13/Exercise13.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercise13 {
     
     public static void main(String[] args) {
-
+        askInput();
     }
 
     public static void askInput() {
@@ -17,8 +17,13 @@ public class Exercise13 {
         for(int y = 0; y < num; y++) {
             System.out.println();
             for(int x = 0; x < num; x++) {
-                
+                if(y == x || x + y == num - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
         }
+        System.out.println();
     }
 }
